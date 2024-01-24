f9f07a45ce234d18e2a8fa6433bbfee428e23ca5
Valeria <Valcon4717@gmail.com>
2024-01-23 21:35:08 -0700

    added hover effect and functional color change

--- portfolio/components/navbar.tsx
+++ portfolio/components/navbar.tsx
@@ -5,13 +5,15 @@ import styles from '@/styles/navbar.module.css';
 const Navbar = (props: { theme:any, toggleTheme:any }) => {
   return (
     <nav className={`${styles.box} ${props.theme}`}>
-      <Link href="/">Home</Link>
-      <Link href="#about">About</Link>
-      <Link href="#experience">Experience</Link>
-      <Link href="#resume">Resume</Link>
-      <button onClick={props.toggleTheme}>
-        Toggle Theme
-      </button>
+      <h4 className={`${styles.title}`}><Link href="/">Valeria Contreras</Link></h4>
+      <h5 className={`${styles.navItems}`}>
+        <Link href="#about">About</Link>
+        <Link href="#experience">Experience</Link>
+        <Link href="#resume">Resume</Link>
+        <button onClick={props.toggleTheme}>
+          COLOR
+        </button>
+      </h5>
     </nav>
   );
 };
