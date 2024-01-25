5faf594a35e517cf098938b3b0e92a754fbbd0ae
Valeria <Valcon4717@gmail.com>
2024-01-24 19:34:21 -0700

    Added theme changer button

--- portfolio/components/navbar.tsx
+++ portfolio/components/navbar.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Link from 'next/link';
 import styles from '@/styles/navbar.module.css';
 
-const Navbar = (props: { theme:any, toggleTheme:any }) => {
+const Navbar = (props: { theme:any }) => {
   return (
     <nav className={`${styles.box} ${props.theme}`}>
       <h4 className={`${styles.title}`}><Link href="/">Valeria Contreras</Link></h4>
@@ -10,9 +10,6 @@ const Navbar = (props: { theme:any, toggleTheme:any }) => {
         <Link href="#about">About</Link>
         <Link href="#experience">Experience</Link>
         <Link href="#resume">Resume</Link>
-        <button onClick={props.toggleTheme}>
-          COLOR
-        </button>
       </h5>
     </nav>
   );
